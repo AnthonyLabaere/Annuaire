5d9bbd34e045dc24aab168eb342e78702b7f34ae
Labaere <anthony.labaere@gmail.com>
2014-02-10 21:13:00 +0100

    Valeurs des filtres par defaut en javascript

--- public/javascripts/filtres-init.js
+++ public/javascripts/filtres-init.js
@@ -1,20 +1,23 @@
-var FILTRE_VILLE_OPTION_PAR_DEFAUT_TEXTE;
-var FILTRE_ANNEEPROMOTION_OPTION_PAR_DEFAUT_TEXTE;
-var FILTRE_ECOLE_OPTION_PAR_DEFAUT_TEXTE;
+var FILTRE_ANNEEPROMOTION_OPTION_PAR_DEFAUT_TEXTE = 'S&eacute;lectionnez l\'Ann&eacute;e de promotion recherch&eacute;e';
+var FILTRE_ECOLE_OPTION_PAR_DEFAUT_TEXTE = 'S&eacute;lectionnez l\'&Eacute;cole recherch&eacute;e';
 var FILTRE_ENTREPRISE_OPTION_PAR_DEFAUT_TEXTE = 'S&eacute;lectionnez l\'Entreprise recherch&eacute;e';
-var FILTRE_SECTEUR_OPTION_PAR_DEFAUT_TEXTE;
-var FILTRE_PAYS_OPTION_PAR_DEFAUT_TEXTE;
+var FILTRE_SECTEUR_OPTION_PAR_DEFAUT_TEXTE = 'S&eacute;lectionnez le Secteur recherch&eacute;';
+var FILTRE_PAYS_OPTION_PAR_DEFAUT_TEXTE = 'S&eacute;lectionnez le Pays recherch&eacute;';
 var FILTRE_VILLE_OPTION_PAR_DEFAUT_TEXTE = 'S&eacute;lectionnez le Ville recherch&eacute;e';
 
-
-//Alimentation du filtre des annees de promotion
+// Alimentation du filtre des annees de promotion
 jsRoutes.controllers.ServiceAnneePromotion
 		.AJAX_listeDesAnneesdePromotion()
 		.ajax(
 				{
 					success : function(data, textStatus, jqXHR) {
-						// console.log(data);
 						var filtre_anneePromotion = HTML('filtre_anneePromotion');
+
+						filtre_anneePromotion_option_par_defaut = document
+								.createElement('option');
+						filtre_anneePromotion_option_par_defaut.innerHTML = FILTRE_ANNEEPROMOTION_OPTION_PAR_DEFAUT_TEXTE;
+						filtre_anneePromotion
+								.appendChild(filtre_anneePromotion_option_par_defaut);
 
 						for ( var element in data) {
 							filtre_anneePromotion.options[filtre_anneePromotion.options.length] = new Option(
@@ -31,6 +34,12 @@ jsRoutes.controllers.ServiceEcole
 					success : function(data, textStatus, jqXHR) {
 						var filtre_ecole = HTML('filtre_ecole');
 
+						filtre_ecole_option_par_defaut = document
+								.createElement('option');
+						filtre_ecole_option_par_defaut.innerHTML = FILTRE_ECOLE_OPTION_PAR_DEFAUT_TEXTE;
+						filtre_ecole
+								.appendChild(filtre_ecole_option_par_defaut);
+
 						for ( var element in data) {
 							filtre_ecole.options[filtre_ecole.options.length] = new Option(
 									data[element]);
@@ -45,6 +54,12 @@ jsRoutes.controllers.ServiceEntreprise
 				{
 					success : function(data, textStatus, jqXHR) {
 						var filtre_entreprise = HTML('filtre_entreprise');
+
+						filtre_entreprise_option_par_defaut = document
+								.createElement('option');
+						filtre_entreprise_option_par_defaut.innerHTML = FILTRE_ENTREPRISE_OPTION_PAR_DEFAUT_TEXTE;
+						filtre_entreprise
+								.appendChild(filtre_entreprise_option_par_defaut);
 
 						for ( var element in data) {
 							filtre_entreprise.options[filtre_entreprise.options.length] = new Option(
@@ -61,6 +76,12 @@ jsRoutes.controllers.ServiceSecteur
 					success : function(data, textStatus, jqXHR) {
 						var filtre_secteur = HTML('filtre_secteur');
 
+						filtre_secteur_option_par_defaut = document
+								.createElement('option');
+						filtre_secteur_option_par_defaut.innerHTML = FILTRE_SECTEUR_OPTION_PAR_DEFAUT_TEXTE;
+						filtre_secteur
+								.appendChild(filtre_secteur_option_par_defaut);
+
 						for ( var element in data) {
 							filtre_secteur.options[filtre_secteur.options.length] = new Option(
 									data[element]);
@@ -76,6 +97,12 @@ jsRoutes.controllers.ServicePays
 					success : function(data, textStatus, jqXHR) {
 						var filtre_pays = HTML('filtre_pays');
 
+						filtre_pays_option_par_defaut = document
+								.createElement('option');
+						filtre_pays_option_par_defaut.innerHTML = FILTRE_PAYS_OPTION_PAR_DEFAUT_TEXTE;
+						filtre_pays
+								.appendChild(filtre_pays_option_par_defaut);
+
 						for ( var element in data) {
 							filtre_pays.options[filtre_pays.options.length] = new Option(
 									data[element]);
@@ -83,7 +110,8 @@ jsRoutes.controllers.ServicePays
 					}
 				});
 
-// Creation et alimentation du filtre des villes en considérant le pays selectionne
+// Creation et alimentation du filtre des villes en considérant le pays
+// selectionne
 function creationAlimentation_filtreVille(pays_nom) {
 	// Creation du filtre
 	villeTd1 = document.createElement('td');
@@ -108,15 +136,15 @@ function creationAlimentation_filtreVille(pays_nom) {
 	villeTr.appendChild(villeTd2);
 
 	HTML('tableau_critere').appendChild(villeTr);
-	
-	// Alimentation du filtre	
+
+	// Alimentation du filtre
 	jsRoutes.controllers.ServiceVille
 			.AJAX_listeDesVillesDuPays(pays_nom)
 			.ajax(
 					{
 						success : function(data, textStatus, jqXHR) {
 							var filtre_ville = HTML('filtre_ville');
-							
+
 							// Suppression des elements existants dans le filtre
 							filtre_ville.innerHTML = "";
 							filtre_ville_option_par_defaut = document
@@ -135,6 +163,6 @@ function creationAlimentation_filtreVille(pays_nom) {
 					});
 }
 
-function suppression_filtreVille(){
-	HTML('tableau_critere').removeChild(HTML('tr_ville'));	
+function suppression_filtreVille() {
+	HTML('tableau_critere').removeChild(HTML('tr_ville'));
 }
