125d8aa29f730f33dd0bbdfab443b6f9d458d9bd
Labaere <anthony.labaere@gmail.com>
2014-02-11 16:42:10 +0100

    Suppression des tests Ajax

--- app/controllers/RoutingAjax.java
+++ app/controllers/RoutingAjax.java
@@ -8,42 +8,12 @@ import play.mvc.Result;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 
-public class Ajax extends Controller {
-
-	public static Result testAjax(String arg) {
-		return ok("Pays : " + arg);
-	}
-
-	@BodyParser.Of(play.mvc.BodyParser.Json.class)
-	public static Result testAjaxEnvoyer() {
-		JsonNode json = request().body().asJson();
-		if (json == null) {
-			return badRequest("Objet Json attendu");
-		} else {
-			String nom = json.findPath("nom").textValue();
-			if (nom == null) {
-				return badRequest("Il manque le parametre [nom]");
-			} else {
-				return testAjaxRecevoir(nom);
-			}
-		}
-
-	}
-
-	public static Result testAjaxRecevoir(String nom) {
-		ObjectNode result = play.libs.Json.newObject();
-		result.put("message", "Salut " + nom + "!");
-		result.put("taille_du_nom", nom.length());
-		return ok(result);
-	}
+public class RoutingAjax extends Controller {
 
 	// Javascript routing
 	public static Result javascriptRoutes() {
 		response().setContentType("text/javascript");
 		return ok(Routes.javascriptRouter("jsRoutes",
-		        controllers.routes.javascript.Ajax.testAjax(),
-		        controllers.routes.javascript.Ajax.testAjaxEnvoyer(),
-		        controllers.routes.javascript.Ajax.testAjaxRecevoir(),
 		        controllers.routes.javascript.ServiceAnneePromotion.AJAX_listeDesAnneesPromotion(),
 		        controllers.routes.javascript.ServiceAnneePromotion.AJAX_listeDesAnneesPromotionSelonCriteres(),
 		        controllers.routes.javascript.ServiceEcole.AJAX_listeDesEcoles(),
